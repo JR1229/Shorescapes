8fde66c41ce5531ea2f2c544c6c777255f9210bb
JR1229 <jdrproductions@hotmail.com>
2026-02-28 17:33:07 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+function togglePanel(i) { const panel = document.getElementById("panel" + i); panel.style.display = panel.style.display === "block" ? "none" : "block"; }
+
+function submitForm() { alert("Thanks! Your booking request has been submitted. We'll contact you within 1 hour."); }
